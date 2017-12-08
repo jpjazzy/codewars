158f383023602e59a9d47401b346cb6ade2bfb08
Jeremy <jpjazzy@hotmail.com>
2017-12-07 17:10:07 -0800

    added reversed strings

--- reversed-strings/solution.js
+++ reversed-strings/solution.js
@@ -1,3 +1,6 @@
-function solution(str){
-  return str.split('').reverse().join('');
+function solution(s){
+  var o = ''; //make blank strings
+  for (var i = s.length - 1; i >= 0; i--) // decrement from end of the string
+    o += s[i]; // grab char at the position and add it to the new variable
+  return o; //return generated string
 }
